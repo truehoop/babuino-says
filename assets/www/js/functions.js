c98404eca213bbdcc652c721498cffe039c3f21c
lake.lordluis@gmail.com <lake.lordluis@gmail.com@f6eb9c61-0808-070a-feb3-ce404a3253c9>
2011-12-30 04:00:05 +0000

    Se agregan imagenes del launcher Se crea archivo para recoger las frases

--- assets/www/js/functions.js
+++ assets/www/js/functions.js
@@ -1,42 +1,54 @@
- var phrases=[["vamos por unas burras", "¿o nomas puros amigos?"],
-              ["tres","cuatro"],
-              ["cinco","seis"]];
- var arrayPosition=0; 
+ var arrayPosition=1; 
 
 function onLoad(){
+	loadPhrasesDiv();
       document.addEventListener("deviceready", onDeviceReady, true);      
       $( "#b-left" ).bind( "click", function(event, ui) {
     	  setDirections("left");
-    	  generateText();
+    	  generateText();    	  	
     	});      
       $( "#b-right" ).bind( "click", function(event, ui) {
     	  setDirections("right");
     	  generateText();
     	});
+    	$( "#b-generate" ).bind( "click", function(event, ui) {
+			navigator.notification.alert("No implemented yet...");
+    	});
       generateText();
  }
  function onDeviceReady(){
-      //navigator.notification.alert("PhoneGap is working");
+      
  }
  function generateText(){	  
-	  document.getElementById("text-top").innerHTML=phrases[arrayPosition][0];
-	  document.getElementById("text-bottom").innerHTML=phrases[arrayPosition][1];
+	  document.getElementById("text-top").innerHTML=
+		  document.getElementById("ph-" + arrayPosition + "_0").textContent;
+	  document.getElementById("text-bottom").innerHTML=
+		  document.getElementById("ph-" + arrayPosition + "_1").textContent;
 	  setTextBottomPosition();
  }
  function setDirections(direction){
-	 if (direction=="left"){
-		 arrayPosition--;
-		 arrayPosition = 
-		 		arrayPosition < 0 ?  (phrases.length -1 ) : arrayPosition--;
-		 }
-	 if (direction=="right"){
-		 arrayPosition++;
-		 arrayPosition = 
-	 		arrayPosition > (phrases.length -1)  ?  0 : arrayPosition++;
+	 switch (direction){
+	 case "left":
+ 		arrayPosition--;
+		arrayPosition = 
+			arrayPosition < 1 ?  ($("#phrases > div").size() / 2 ) : arrayPosition--;
+		 break;
+	 case "right":
+	 	arrayPosition++;
+ 		arrayPosition = 
+ 			arrayPosition > ($("#phrases > div").size() / 2)  ?  1 : arrayPosition++;
+		break;
 	 }
  }
  function setTextBottomPosition(){
 	 document.getElementById("text-bottom").style.top= 300 -10
 	  - document.getElementById("text-bottom").offsetHeight
 	  - document.getElementById("text-top").offsetHeight + "px";
+ }
+ function loadPhrasesDiv(){
+	 var req = new XMLHttpRequest();
+	 req.open("GET", "html/phrases.html", false);
+	 req.send(null);
+	 var page = req.responseText;
+	 document.getElementById("phrases").innerHTML = page;
  }
