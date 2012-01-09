3666397732230bbe953858d02226e1bcae838078
lake.lordluis@gmail.com <lake.lordluis@gmail.com@f6eb9c61-0808-070a-feb3-ce404a3253c9>
2012-01-09 18:16:10 +0000

    Se agrega la funcion para convertir el canvas generado a imagen Se cambia el font-shadow por stroke Se corrige el Manifest para que agregue lectura - escritura

--- assets/www/js/functions.js
+++ assets/www/js/functions.js
@@ -1,7 +1,21 @@
- var arrayPosition=1; 
+/**
+ * La posición inicial para obtener los textos
+ */
+var arrayPosition=1;
+/**
+ * Verifica que la funcion notification exista (para debugueo en navegador)
+ */
+var notificationDefined = navigator.notification != null;
+
+var oImgJPEG = null;
+
+/**
+ * Obtiene los textos iniciales y agrega los eventos
+ */
 function onLoad(){
 	loadPhrasesDiv();
-      document.addEventListener("deviceready", onDeviceReady, true);      
+      document.addEventListener("deviceready", onDeviceReady, true);  
+      
       $( "#b-left" ).bind( "click", function(event, ui) {    	  
     	  setDirections("left");
     	});      
@@ -9,25 +23,52 @@ function onLoad(){
     	  setDirections("right");
     	});
     	$( "#b-generate" ).bind( "click", function(event, ui) {
-    		onMenuKeyDown();
+    		generateCanvas();   		  		
     	});
-      generateText();
+      getText();
  }
+
+/**
+ * Al estar el dispositivo listo, agrega los eventos extras
+ */
  function onDeviceReady(){
 	 document.addEventListener("menubutton", onMenuKeyDown, false);
  }
+ 
+ /**
+  * Evento al presionar el botón menú del dispositivo
+  */
  function onMenuKeyDown() {
-	 navigator.notification.vibrate(200);
-	 navigator.notification.alert("Pr&oacute;ximamente...");
+	 if (notificationDefined)
+		 navigator.notification.vibrate(200);
+	 generateCanvas();
  }
 
- function generateText(){	  
+ /**
+  * Obtiene el texto a mostrar 
+  */
+ function getText(){	  
 	 $("#text-top").html($("#ph-" + arrayPosition + "_0").text());
 	 $("#text-bottom").html($("#ph-" + arrayPosition + "_1").text());
-	  setTextPosition();
+	  setTextPosition(20);
  }
+ 
+ /**
+  * Reposiciona el texto y apaga controles para generar el canvas
+  */
+ function generateCanvas(){
+	 setTextPosition(-5);
+	 hideDivs();
+	 $('body').html2canvas();  
+ }
+ 
+ /**
+  * La dirección en la que mostrará el siguiente texto
+  * @param direction la dirección del botón presionado (izquierda o derecha)
+  */
  function setDirections(direction){
-	 navigator.notification.vibrate(200);
+	 if (notificationDefined)
+		 navigator.notification.vibrate(200);
 	 switch (direction){
 	 case "left":
  		arrayPosition--;
@@ -40,23 +81,93 @@ function onLoad(){
  			arrayPosition > ($("#phrases > div").size() / 2)  ?  1 : arrayPosition++;
 		break;
 	 }
-	 generateText();
+	 getText();
  }
- function setTextPosition(){
+ 
+ /**
+  * Calcula la posición del texto a desplegar
+  * @param extraPx la posición en pixeles del texto inferior 
+  */
+ function setTextPosition(extraPx){
 	 /*Se coloca la posicion del texto inferior*/
-	 $("#text-bottom").css("top", $("#imagen").height() 
-			 - $("#text-bottom").height() + 20 + "px");
+	 $("#text-bottom").css("top", $("#background").height() 
+			 - $("#text-bottom").height() + extraPx + "px");
 	 /*Se calcula la posicion de los textos para colocarlos en medio*/
 	 $("#text-top").css("left", 
 		 ((document.documentElement.clientWidth / 2) 
-			-($("#imagen").width() / 2)) + "px");
+			-($("#background").width() / 2)) + "px");
 	 $("#text-bottom").css("left", 
 		 ((document.documentElement.clientWidth / 2) 
-			-($("#imagen").width() / 2)) + "px");
+			-($("#background").width() / 2)) + "px");
  }
+ 
+ /**
+  * Obtiene el listado de frases a desplegar
+  */
  function loadPhrasesDiv(){
 	 var req = new XMLHttpRequest();
 	 req.open("GET", "html/phrases.html", false);
 	 req.send(null);
 	 $("#phrases").html(req.responseText);
  }
+ 
+ /**
+  * Genera una imagen a partir de un objeto canvas
+  * @param oCanvas el objeto canvas del cual se obtiene la imagen
+  */
+ function saveCanvas(oCanvas){
+	 $("#imagen2").html("");
+	 oImgJPEG = Canvas2Image.saveAsBMP(oCanvas, true, 300, 300);
+	 $("#imagen2").html(oImgJPEG);
+	 //window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+ } 
+ 
+ /**
+  * Oculta los divs indicados para que el canvas no los genere
+  */
+ function hideDivs(){
+	 $('#buttons').toggle();
+	 $('#title').toggle();
+ }
+ 
+ /**
+  * Genera el objeto canvas que contendra la imagen a compartir
+  */
+ 
+ (function( $ ){
+	    $.fn.html2canvas = function(options) {
+			$('#canvas').html("");
+	        html2canvas.Preload(this[0], $.extend({
+	            complete: function(images){
+	                var queue = html2canvas.Parse(this[0], images, options),
+	                $canvas = $(html2canvas.Renderer(queue, options));           
+	          	    $('#canvas').html($canvas);
+	          	    hideDivs();
+	        	  	setTextPosition(20);
+	        	  	saveCanvas($("#canvas").children()[0]);
+	            }
+	        }, options));	       
+	    };
+	})( jQuery );
+ /*
+ function gotFS(fileSystem) {
+	 console.log("quiere obtener el archivo");
+     fileSystem.root.getFile("Pictures/sbabuino.bmp", {create: true}, gotFileEntry, fail);
+ }
+
+ function gotFileEntry(fileEntry) {
+	 console.log("obtuvo el archivo");
+     fileEntry.createWriter(gotFileWriter, fail);
+ }
+
+ function gotFileWriter(writer) {
+     writer.onwrite = function(evt) {
+         console.log("write success");
+     };
+     writer.write(oImgJPEG);
+ }
+
+ function fail(error) {
+     console.log(error.code);
+ }
+*/
